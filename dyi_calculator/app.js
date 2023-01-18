d0afe49f99150d459247a39195457f956979fecb
NikaGreen <40990893+NikaGreen@users.noreply.github.com>
2023-01-18 21:10:53 +0100

    Add files via upload

---
+++ dyi_calculator/app.js
@@ -0,0 +1,80 @@
+const calculator = document.querySelector('.calculator')
+const display = calculator.querySelector('.displayNumbers')
+const keys = calculator.querySelector('.keys')
+
+const calculate = (n1, operator, n2) => {
+    let result = ''
+    if (operator === 'add') {
+      result = parseFloat(n1) + parseFloat(n2)
+    } else if (operator === 'subtract') {
+      result = parseFloat(n1) - parseFloat(n2)
+    } else if (operator === 'multiply') {
+      result = parseFloat(n1) * parseFloat(n2)
+    } else if (operator === 'divide') {
+      result = parseFloat(n1) / parseFloat(n2)
+    }else if (operator === 'sqr'){
+      result = parseFloat(n1)* parseFloat(n2)
+    }else if (operator === 'sqrt'){
+      result = Math.sqrt(n1)
+    }else if (operator === 'rmdr'){
+      result = parseFloat(n1) % parseFloat(n2)
+  }
+  
+    return result
+  }
+  
+  keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+      const key = e.target
+      const action = key.dataset.action
+      const keyContent = key.textContent
+      const displayedNum = display.textContent
+      const previousKeyType = calculator.dataset.previousKeyType
+  
+      Array.from(key.parentNode.children)
+        .forEach(k => k.classList.remove('is-depressed'))
+  
+      if (!action) {
+        if (displayedNum === '0' || previousKeyType === 'operator') {
+          display.textContent = keyContent
+        } else {
+          display.textContent = displayedNum + keyContent
+        }
+      }
+  
+      if (action === 'decimal') {
+        display.textContent = displayedNum + '.'
+      }
+  
+      if (
+        action === 'add' ||
+        action === 'subtract' ||
+        action === 'multiply' ||
+        action === 'divide'||
+        action === 'sqr'||
+        action === 'sqrt'||
+        action === 'rmndr'
+      ) {
+        key.classList.add('is-depressed')
+        calculator.dataset.previousKeyType = 'operator'
+        calculator.dataset.firstValue = displayedNum
+        calculator.dataset.operator = action
+      }
+  
+      if (action === 'clear') {
+        console.log('clear key!')
+        display.textContent = 0
+      }
+  
+      if (action === 'calculate') {
+        const firstValue = calculator.dataset.firstValue
+        const operator = calculator.dataset.operator
+        const secondValue = displayedNum
+  
+        display.textContent = calculate(firstValue, operator, secondValue)
+      }
+    }
+  })
+
+
+  
